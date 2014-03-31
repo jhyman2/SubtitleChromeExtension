fad5318eb4a8c7707cae61c5aa95466e2695cdeb
Jamison Hyman <jhyman2@umbc.edu>
2014-03-31 18:44:40 -0400

    Updates, has time and stuff.

--- background.js
+++ background.js
@@ -1,10 +1,20 @@
 //create an element
-var $element = $('<div class="draggableResizable" />').text('some text');
+var $element = $('<div id="text" class="draggableResizable" />').text("Some balls");
 
 //make it "draggable" and "resizable"
 $element.draggable();//.resizable();
-    
+
 //append it to the DOM
 $("body").append($element);
+var t = 10;
 
-//$("body").append('Test');
+while (t != 0){
+setInterval(function(){addBalls()},1000);
+t = t - 1;
+}
+
+function addBalls(){
+var d=new Date();
+var f=d.toLocaleTimeString();
+document.getElementById("text").innerHTML = f;
+}
