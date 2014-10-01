1609199c6758cc3995cee687c567d836914c0f6b
Jamison Hyman <jhyman2@umbc.edu>
2014-09-30 22:26:59 -0400

    Parsing SRT file and creating subtitle objects

--- content_script.js
+++ content_script.js
@@ -1,31 +1,50 @@
 // Reads in text file and console logs it
- var fullText;
- var startTimes;
- var endTimes;
- var words;
-// var xhr = new XMLHttpRequest();
-// xhr.open('GET', 'http://tutorrow.com/subtle_subtitles/example.srt');
-// xhr.onreadystatechange = function()
-// {
-//     if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
-//     {
-//         fullText = xhr.responseText;
-//         //... The content has been read in xhr.responseText
-//     }
-// };
-// xhr.send();
-
+var finalSubs = [];
 $.ajax({
   url: "http://tutorrow.com/subtle_subtitles/example.srt",
   dataType: "html",
-  success: function(data){
-    var lines = data.split('\n');
 
-    lines.forEach(function(currentLine) {
-        if (/(.*:.*:.*,.*) --> (.*)/.test(currentLine)){
-            console.log(currentLine);
+  // called when remote file is grabbed
+  success: function(data){
+
+    // lines is an array of every line in the subtitle file
+    var lines = data.split('\n');
+    var numLines = lines.length;
+    var i;
+
+    // Regular expressions to match times and non-whitespace lines
+    var patt = new RegExp("(.*:.*:.*,.*) --> (.*)");
+    var patt2 = new RegExp("S+");
+
+    // loop through each line 
+    // if line has a time in it, grab it and then grab each line after until a blank line is reached
+    for (i = 0; i < numLines; i++){
+
+        // if line has times on it
+        if (patt.test(lines[i])){
+            var twoTimes = lines[i].split(" --> ");
+            var currentSubText = "";
+            i = i + 1;
+
+            // read subtitle text until a blank line is reached
+            while(patt2.test(lines[i])){
+                currentSubText = currentSubText.concat(lines[i]);
+                i = i +1;
+            }
+
+            // if text was read, then create a new subtitle object and add it to the finalSubs array
+            if (currentSubText){
+                var sub = {
+                    startTime: twoTimes[0],
+                    endTime: twoTimes[1],
+                    text: currentSubText
+                };
+                finalSubs.push(sub);
+            }
         }
-    }); 
+    }
+    // log the subtitles
+    console.log(finalSubs);
   }
 });
 
