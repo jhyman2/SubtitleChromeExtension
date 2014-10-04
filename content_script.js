ffe131c0c1b573a84051fa9cc7100a49b65e26bf
Jamison Hyman <jhyman2@umbc.edu>
2014-10-04 05:01:08 -0400

    Parsing actually works now.  Regex accounts for trailing whitespace.

--- content_script.js
+++ content_script.js
@@ -6,31 +6,31 @@ $.ajax({
 
   // called when remote file is grabbed
   success: function(data){
-
     // lines is an array of every line in the subtitle file
     var lines = data.split('\n');
     var numLines = lines.length;
     var i;
-    console.log("NUMLINES", numLines);
+    
     // Regular expressions to match times and non-whitespace lines
-    var patt = new RegExp("(.*:.*:.*,.*) --> (.*)");
-    var patt2 = new RegExp("S+");
+    var patt = new RegExp("(.*:.*:.*,.*) --> (.*)[ \t\n]*");
+    var patt2 = new RegExp(".+");
 
     var count = 0;
 
     // loop through each line 
     // if line has a time in it, grab it and then grab each line after until a blank line is reached
     for (i = 0; i < numLines; i++){
-
         // if line has times on it
         if (patt.test(lines[i])){
             count++;
             var twoTimes = lines[i].split(" --> ");
             var currentSubText = "";
             i = i + 1;
-
             // read subtitle text until a blank line is reached
-            while(patt2.test(lines[i])){
+            while(patt2.test(lines[i]) && lines[i]){
+                if (currentSubText != ""){
+                    currentSubText = currentSubText.concat("\n");
+                }
                 currentSubText = currentSubText.concat(lines[i]);
                 i = i +1;
             }
@@ -42,8 +42,8 @@ $.ajax({
                     endTime: twoTimes[1],
                     text: currentSubText
                 };
+                finalSubs.push(sub);
             }
-            finalSubs.push(sub);
         }
     }
     // log the subtitles
@@ -63,7 +63,14 @@ $("body").append($element);
 
 // $(document).ready(function(){
 //   $("#text").click(function(){
-//     document.getElementById("text").innerHTML += "That tickles!";
+//     var timer = 0.0;
+//     var count = 0;
+//     document.getElementById("text").innerHTML = "Subtitles starting...";
+
+//     while (timer < finalSubs[finalSubs.length - 1]){
+//         timer = finalSubs[count++].startTime;
+//         window.setTimeout("javascript function", timer);
+//     }
 //   });
 // });
 
