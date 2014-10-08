de991a61367c5e5a7e48581b883ee1162ef07e2d
Joseph Kahn <JosephBKahn@gmail.com>
2014-10-08 02:10:45 -0400

    workin

--- background.js
+++ background.js
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Called whenUpdated creates a new tab.
 chrome.tabs.onUpdated.addListener(function(activeTab) {
 
     chrome.browserAction.setPopup({
