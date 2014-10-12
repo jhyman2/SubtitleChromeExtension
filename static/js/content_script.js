f8fbc7b9805fcf505de07a56ef8e9c5270e1c447
Joseph Kahn <JosephBKahn@gmail.com>
2014-10-12 04:15:17 -0400

    backbone working

--- static/js/content_script.js
+++ static/js/content_script.js
@@ -1,18 +1,28 @@
-function startSubs () {
-    doInCurrentTab( function(tab){ chrome.tabs.executeScript(tab.id, { file: 'static/js/subs.js' }); } );
-}
+define(["backbone"], function(Backbone) {
 
-function doInCurrentTab(tabCallback) {
-    chrome.tabs.query(
-        { currentWindow: true, active: true },
-        function (tabArray) { tabCallback(tabArray[0]); }
-    );
-}
+    var App = Backbone.View.extend({
 
-function init() {
-    $('#start-subtitles').click(startSubs);
-}
+        initialize: function(){
 
+            function startSubs () {
+                if (!this.beenClicked) {
+                    this.beenClicked = true;
+                    doInCurrentTab( function(tab){ console.log('all the buttons');chrome.tabs.executeScript(tab.id, { file: 'static/js/subs.js' }); } );
+                }
+            }
 
-document.addEventListener('DOMContentLoaded', init);
+            function doInCurrentTab(tabCallback) {
+                chrome.tabs.query(
+                    { currentWindow: true, active: true },
+                    function (tabArray) { tabCallback(tabArray[0]); }
+                );
+            }
 
+            $( document ).ready(function() {
+                this.beenClicked = false;
+                $('#start-subtitles').click(startSubs);
+            });
+        }
+    });
+    return App;
+});
